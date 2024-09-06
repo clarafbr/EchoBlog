aafa5871fa43c9f4536c70d223435561658132dc
Maria Clara <mclarafarias2@gmail.com>
2024-09-06 08:58:53 -0300

    rota de deletar postagem finalizada

--- backend/src/controllers/postagemController.js
+++ backend/src/controllers/postagemController.js
@@ -19,6 +19,7 @@ const updateTarefaSchema = z.object({
     autor: z.string().min(3, {message: "O autor deve ter pelo menos 3 caracteres"}),
 })
 
+//PUXAR TODAS AS POSTAGENS
 export const getAll = async (request, response) => {
     const page = parseInt(request.query.page) || 1
     const limit = parseInt(request.query.limit) || 10
@@ -45,6 +46,7 @@ export const getAll = async (request, response) => {
     }
 }
 
+//CRIAR UMA NOVA POSTAGEM
 export const create = async (request, response)=> {
     
     //implementar a validação 
@@ -76,6 +78,7 @@ export const create = async (request, response)=> {
     }
 }
 
+//PUXAR POSTAGEM POR ID
 export const getPostagem = async(request, response) => {
     const paramValidator = getSchema.safeParse(request.params)
     if(! paramValidator.success){
@@ -99,6 +102,7 @@ export const getPostagem = async(request, response) => {
     }
 }
 
+//ATUALIZAR POSTAGEM
 export const updatePostagem = async(request, response) => {
     const paramValidator = getSchema.safeParse(request.params)
     if(! paramValidator.success){
@@ -152,5 +156,28 @@ export const updatePostagem = async(request, response) => {
         response.status(200).json({message: "Postagem atualizada com sucesso"})
     } catch (error) {
         response.status(500).json({message: "Erro ao atualizar postagem"})
+    }
+}
+
+export const deletePostagem = async(request, response) => {
+    const paramValidator = getSchema.safeParse(request.params)
+    if(! paramValidator.success){
+        response.status(400).json({
+            message: "Número de identificação está invalido",
+            detalhes: formatZodError(paramValidator.error)
+        })
+        return
+    }
+    const {id} = request.params
+
+    try {
+        const postagem = await Postagem.destroy({where: { id }})
+        if(postagem === null){
+            response.status(404).json({message: "postagem não encontrada"})
+            return
+        }
+        response.status(200).json({message: "Postagem deletada com sucesso"})
+    } catch (error) {
+        response.status(500).json({message: "Erro ao buscar postagem"})
     }
 }
--- backend/src/router/postagemRouter.js
+++ backend/src/router/postagemRouter.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getAll, create, getPostagem, updatePostagem} from "../controllers/postagemController.js";
+import { getAll, create, getPostagem, updatePostagem, deletePostagem} from "../controllers/postagemController.js";
 
 const router = Router()
 
@@ -7,5 +7,6 @@ router.get("/", getAll)
 router.post("/", create)
 router.get("/:id", getPostagem)
 router.put("/:id", updatePostagem)
+router.delete("/:id", deletePostagem)
 
 export default router
